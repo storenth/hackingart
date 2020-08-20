dc70be78c06d3f9dbce1078bcc7c1d907cc77c10
Kirill Zhdanov <storenth.catena@gmail.com>
2020-08-20 22:03:56 +0700

    addressof2 printf refactor with 'p'

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hackingart
+[Originally](https://nostarch.com/hacking2.htm) downloaded these sources required improvement which is the purpose of this repository to track made changes.
+
+# Acknowledgement
+This repository for education and personal, non-commercial use only with no liabilities.
+
+# Notes
+1. `0x%08x` will be replaced with `%p` in case of `printf` of the pointers.
+2. `printf("text\n")` replaced with `puts("text")`
--- addressof2.c
+++ addressof2.c
@@ -6,11 +6,11 @@ int main() {
 
    int_ptr = &int_var; // put the address of int_var into int_ptr
 
-   printf("int_ptr = 0x%08x\n", int_ptr);
-   printf("&int_ptr = 0x%08x\n", &int_ptr);
+   printf("int_ptr = %p\n", int_ptr);
+   printf("&int_ptr = %p\n", &int_ptr);
    printf("*int_ptr = 0x%08x\n\n", *int_ptr);
 
-   printf("int_var is located at 0x%08x and contains %d\n", &int_var, int_var);
-   printf("int_ptr is located at 0x%08x, contains 0x%08x, and points to %d\n\n", 
+   printf("int_var is located at %p and contains %d\n", &int_var, int_var);
+   printf("int_ptr is located at %p, contains %p, and points to %d\n\n", 
       &int_ptr, int_ptr, *int_ptr);
 }
