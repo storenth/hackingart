293ea1e31528278559fbca77c749d83344df860c
Kirill Zhdanov <storenth.catena@gmail.com>
2020-08-24 23:11:30 +0700

    Add array vs pointer explanation example

---
+++ learning-c-with-gdb-minimal.c
@@ -0,0 +1,9 @@
+/* Arrays are not pointers Explanation minimum example 
+But an array can automatically "decay" into a pointer.
+https://www.recurse.com/blog/5-learning-c-with-gdb
+https://stackoverflow.com/a/63564607/6485674 */
+int main()
+{
+    int a[] = {1,2,3};
+    return 0;
+}
