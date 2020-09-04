04d951947fdcda02ea5c5688b5f7db25c5c2bbc5
Kirill Zhdanov <storenth.catena@gmail.com>
2020-09-05 00:02:02 +0700

    Replace buffers because two follows one

--- overflow_example.c
+++ overflow_example.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 	printf("[BEFORE] value is at %p and is %d (0x%08x)\n", &value, value, value);
 
 	printf("\n[STRCPY] copying %d bytes into buffer_two\n\n",  strlen(argv[1]));
-	strcpy(buffer_two, argv[1]); /* copy first argument into buffer_two */
+	strcpy(buffer_one, argv[1]); /* copy first argument into buffer_one */
 
 	printf("[AFTER] buffer_two is at %p and contains \'%s\'\n", buffer_two, buffer_two);
 	printf("[AFTER] buffer_one is at %p and contains \'%s\'\n", buffer_one, buffer_one);
