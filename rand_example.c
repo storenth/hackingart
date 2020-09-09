7fd522c3fdd7df27f5110d145641825d6b5f2afa
Kirill Zhdanov <storenth.catena@gmail.com>
2020-09-09 22:38:43 +0700

    Update pseudo-random with appropriate import

--- rand_example.c
+++ rand_example.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
 	int i;
 	printf("RAND_MAX is %u\n", RAND_MAX);
 	srand(time(0));
 
-	printf("random values from 0 to RAND_MAX\n");
+	puts("random values from 0 to RAND_MAX");
 	for(i=0; i < 8; i++)
 		printf("%d\n", rand());
-	printf("random values from 1 to 20\n");
+	puts("random values from 1 to 20");
 	for(i=0; i < 8; i++)
 		printf("%d\n", (rand()%20)+1);
 }
